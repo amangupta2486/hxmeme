45f9167920c9ffb8ea2e4ad6d0e4fd3088c54806
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2021-02-13 00:21:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-sed 's/#.*//' requirements1.txt | xargs sudo apt-get install
+#sed 's/#.*//' requirements1.txt | xargs sudo apt-get install
+
+sudo apt-get install sqlite3
+sudo apt-get install aiosqlite
+sudo apt-get install databases
+sudo apt-get install fastapi
+sudo apt-get install Hypercorn
+sudo apt-get install Jinja2
+sudo apt-get install pydantic
+sudo apt-get install requests
+sudo apt-get install SQLAlchemy
+sudo apt-get install tortoise-orm
+sudo apt-get install uvicorn
 
