68bfbde99ff35f5a6f11081ebd07ace62475446d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2021-02-13 23:10:14 +0530

    Update test_server.sh

--- test_server.sh
+++ test_server.sh
@@ -5,13 +5,14 @@
 
 # cd to the cloned repo directory
 
-sudo apt update
-
-sudo apt-get install -y python3.6
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
 
 sudo apt update -y
 
 sudo apt install python3.8
+
+apt list | grep python3.8
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 
 
